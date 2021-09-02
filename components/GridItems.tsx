82b7b5325ba54f350d651024776317e4bafa2cb4
Joss <josdesi@gmail.com>
2021-09-01 23:22:04 -0500

    Grid items layout

---
+++ components/GridItems.tsx
@@ -0,0 +1,33 @@
+import { SimpleGrid } from '@chakra-ui/react'
+import React from 'react'
+import Card from './Card'
+
+let dataDummy: any[]= new Array<any>();
+    ([...Array(100)]).map((item:number) => {
+        dataDummy.push({
+            id:item
+        })
+    })
+export default function GridItems() {
+    return (
+        <div>
+            <SimpleGrid spacing={10} minChildWidth="250px">
+
+                {
+                    dataDummy.map((item) => {
+                        return (
+                            <Card
+                                key={item.id}
+                                image="https://bit.ly/2k1H1t6"
+                                title="Mission_Name"
+                                subtitle="Rocket_Name"
+                                link="http://www.google.com"
+                                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam obcaecati dicta quis quo praesentium quos, voluptatibus officia. Vel iste ea amet sapiente eveniet qui, tempora aliquam nesciunt asperiores, voluptatem quasi?"
+                            />
+                        )
+                    })
+                }
+            </SimpleGrid>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,18 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, SimpleGrid } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Card from '../components/Card'
+import GridItems from '../components/GridItems'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <>
-      {
-        ([...Array(100)]).map(() => {
-          return (
-            <Card
-              image="https://bit.ly/2k1H1t6"
-              title="Mission_Name"
-              subtitle="Rocket_Name"
-              link="http://www.google.com"
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam obcaecati dicta quis quo praesentium quos, voluptatibus officia. Vel iste ea amet sapiente eveniet qui, tempora aliquam nesciunt asperiores, voluptatem quasi?"
-            />
-          )
-        })
-      }
+    <Box p={10} pl="5%" pr="5%" justifyContent='center'>
+
+      <GridItems />      
+    </Box>
     </>
   )
 }
